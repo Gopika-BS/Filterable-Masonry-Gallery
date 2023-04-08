912102d6dd44ee53796ece0de8c71bf918872185
Gopika B.S <letters.to.gopika@gmail.com>
2023-04-08 12:32:34 +0530

    Added masonry gallery view

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 (function(){
     const buttons = document.querySelectorAll(".btn")
     const storeImage = document.querySelectorAll(".store-items")
+    const modalClose = document.querySelector(".modal-close")
     
     buttons.forEach((button) => {
         button.addEventListener('click',(e) => {
             e.preventDefault()
+            document.querySelector('.active').classList.remove('active')
+            e.target.classList.add('active')
             const filter = e.target.dataset.filter
             
             storeImage.forEach((item) => {
@@ -21,5 +24,18 @@
                 }
             })
         })
+    })
+    storeImage.forEach((img) => {
+        img.addEventListener('click',(e) => {
+            e.preventDefault()
+            const imageUrl = e.originalTarget.src;
+            console.log(imageUrl);
+            document.getElementById("modalImage").src = imageUrl; 
+            document.getElementById("modal").classList.add('show') ; 
+        })
+    })
+    modalClose.addEventListener('click',() => {
+        document.getElementById("modalImage").src = "imageUrl"; 
+        document.getElementById("modal").classList.remove('show') 
     })
 })();
